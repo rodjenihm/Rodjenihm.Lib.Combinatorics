b80dd9db5156a1bd38c59fce21e9d0fa34dca0c0
Данијел Митровић <rodjenihm@outlook.com>
2019-03-24 22:15:40 +0100

    Update name of test methods. Add more test cases

--- Rodjenihm.Lib.Combinatorics.Test/PermutationsTests.cs
+++ Rodjenihm.Lib.Combinatorics.Test/PermutationsTests.cs
@@ -1,10 +1,12 @@
 using NUnit.Framework;
 using Rodjenihm.Lib.Combinatorics;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace Tests
 {
+    [TestFixture]
     public class PermutationsTests
     {
         [SetUp]
@@ -14,7 +16,7 @@ namespace Tests
 
         [Test]
         [TestCaseSource(typeof(PermutationsTestsData), nameof(PermutationsTestsData.InputWithZeroRepetitionsTestCases))]
-        public void _InputWithZeroRepetitions_GeneratesAllPermutations<T>(IEnumerable<T> input, List<List<T>> expected)
+        public void InputWithZeroRepetitions_GeneratesAllPermutations<T>(IEnumerable<T> input, List<List<T>> expected)
         {
             var permutations = new Permutations<T>(input);
 
@@ -25,7 +27,7 @@ namespace Tests
 
         [Test]
         [TestCaseSource(typeof(PermutationsTestsData), nameof(PermutationsTestsData.InputWithOneRepetitionsTestCases))]
-        public void _InputWithOneRepetitions_GeneratesAllPermutations<T>(IEnumerable<T> input, List<List<T>> expected)
+        public void InputWithOneRepetitions_GeneratesAllPermutations<T>(IEnumerable<T> input, List<List<T>> expected)
         {
             var permutations = new Permutations<T>(input);
 
@@ -36,13 +38,38 @@ namespace Tests
 
         [Test]
         [TestCaseSource(typeof(PermutationsTestsData), nameof(PermutationsTestsData.InputWithTwoRepetitionsTestCases))]
-        public void _InputWithTwoRepetitions_GeneratesAllPermutations<T>(IEnumerable<T> input, List<List<T>> expected)
+        public void InputWithTwoRepetitions_GeneratesAllPermutations<T>(IEnumerable<T> input, List<List<T>> expected)
         {
             var permutations = new Permutations<T>(input);
 
             var actual = new List<List<T>>(permutations);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InputHasOneElement_GeneratesOnePermutation()
+        {
+            int[] input = new int[] { 1 };
+
+            var permutations = new Permutations<int>(input);
+
+            var expected = new List<List<int>>
+            {
+                new List<int> { 1 }
+            };
+
+            var actual = new List<List<int>>(permutations);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InputIsNull_ThrowsArgumentNullException()
+        {
+            int[] input = null;
+
+            Assert.Throws<ArgumentNullException>(() => new Permutations<int>(input));
         }
     }
 }
